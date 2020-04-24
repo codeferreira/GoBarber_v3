77d85d6fdf773adede3118d94fbec90554375209
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-04-24 12:03:09 -0300

    chore: Add created_at and updated_at to appointment

--- backend/src/database/migrations/1587663851992-CreateAppointments.ts
+++ backend/src/database/migrations/1587663851992-CreateAppointments.ts
@@ -24,6 +24,16 @@ export default class CreateAppointments1587663851992
             type: 'timestamp with time zone',
             isNullable: false,
           },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
         ],
       }),
     );
