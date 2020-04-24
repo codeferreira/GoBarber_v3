e12caa99da464dfdbfafc79b2fb51edd3eb5cc5d
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-04-24 12:04:56 -0300

    chore: Change appointment provider to provider_id

--- backend/src/routes/appointments.routes.ts
+++ backend/src/routes/appointments.routes.ts
@@ -16,7 +16,7 @@ appointmentsRoutes.get('/', async (request, response) => {
 
 appointmentsRoutes.post('/', async (request, response) => {
   try {
-    const { provider, date } = request.body;
+    const { provider_id, date } = request.body;
 
     const parsedDate = parseISO(date);
 
@@ -24,7 +24,7 @@ appointmentsRoutes.post('/', async (request, response) => {
 
     const appointment = await createAppointment.execute({
       date: parsedDate,
-      provider,
+      provider_id,
     });
 
     return response.json(appointment);
