797f5ee5bcf4d7e689d56c06f1878f439dd1e5a2
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-05-04 16:38:42 -0300

    reacftor: change routes function to controllers

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,35 +1,14 @@
 import { Router } from 'express';
-import { parseISO } from 'date-fns';
-import { container } from 'tsyringe';
-
-import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
+import AppointmentsController from '../controllers/AppointmentsContrroller';
+
 const appointmentsRoutes = Router();
+const appointmentsController = new AppointmentsController();
 
 appointmentsRoutes.use(ensureAuthenticated);
 
-// appointmentsRoutes.get('/', async (request, response) => {
-//   const appointmentsRepository = getCustomRepository(AppointmentsRepository);
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
-appointmentsRoutes.post('/', async (request, response) => {
-  const { provider_id, date } = request.body;
-
-  const parsedDate = parseISO(date);
-
-  const createAppointment = container.resolve(CreateAppointmentService);
-
-  const appointment = await createAppointment.execute({
-    date: parsedDate,
-    provider_id,
-  });
-
-  return response.json(appointment);
-});
+appointmentsRoutes.post('/', appointmentsController.create);
 
 export default appointmentsRoutes;
