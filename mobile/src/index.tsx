9ce5ee5b832a77df8d177a90bda51d0382ad91fe
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-05-11 20:45:20 -0300

    chore(mobile): add statusbar basic config

--- mobile/src/index.tsx
+++ mobile/src/index.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, StatusBar } from 'react-native';
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
-  return <View />;
+  return (
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#312e38" />
+      <View style={{ flex: 1, backgroundColor: '#312e38' }} />
+    </>
+  );
 };
 
 export default App;
