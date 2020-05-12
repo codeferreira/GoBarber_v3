37d15f79ef27a928bdc983861e1d157c56ee8f82
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-05-11 21:31:39 -0300

    chore(mobile): config react navigation

---
+++ mobile/src/pages/SignUp/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Image } from 'react-native';
+
+import { Container } from './styles';
+
+import logoImg from '../../assets/logo.png';
+
+const SignUp: React.FC = () => {
+  return (
+    <Container>
+      <Image source={logoImg} />
+    </Container>
+  );
+};
+
+export default SignUp;
