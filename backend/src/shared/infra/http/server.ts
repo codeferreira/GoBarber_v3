22537acd254bedd6eef6423ffd3acd68312baedd
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2020-05-24 19:20:31 -0300

    refactor(api): change upload folder object name

--- backend/src/shared/infra/http/server.ts
+++ backend/src/shared/infra/http/server.ts
@@ -16,7 +16,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use('/files', express.static(uploadConfig.directory));
+app.use('/files', express.static(uploadConfig.tempFolder));
 app.use(routes);
 
 app.use(
