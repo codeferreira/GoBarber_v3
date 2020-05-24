28edaa49b7c5d7e680fa33f62130919f86e9abd2
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2020-05-24 19:19:14 -0300

    chore(backend): change folder to upload config

--- backend/src/config/upload.ts
+++ backend/src/config/upload.ts
@@ -5,7 +5,8 @@ import multer from 'multer';
 const tempFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tempFolder,
+  tempFolder,
+  uploadFolder: path.resolve(tempFolder, 'uploads'),
   storage: multer.diskStorage({
     destination: tempFolder,
     filename(request, file, callback) {
