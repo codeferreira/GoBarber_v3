1e426d83a06c0599c9bbbc01075205f118d6479d
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2020-05-24 08:32:53 -0300

    chore(backend): create a fake users repository to test suit

---
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -0,0 +1,41 @@
+import { uuid } from 'uuidv4';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+import User from '../../infra/typeorm/entities/User';
+
+class UsersRepository implements IUsersRepository {
+  private users: User[] = [];
+
+  public async create(userData: ICreateUserDTO): Promise<User> {
+    const user = new User();
+
+    Object.assign(user, { id: uuid() }, userData);
+
+    this.users.push(user);
+
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const foundedUser = this.users.find(user => user.id === id);
+
+    return foundedUser;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const foundedUser = this.users.find(user => user.email === email);
+
+    return foundedUser;
+  }
+
+  public async save(user: User): Promise<User> {
+    const findIndex = this.users.findIndex(findUser => findUser.id === user.id);
+
+    this.users[findIndex] = user;
+
+    return user;
+  }
+}
+
+export default UsersRepository;
