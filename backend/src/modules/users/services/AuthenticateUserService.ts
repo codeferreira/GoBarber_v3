9cf5f1ebdd7f85f0f4c0b8d0cf297235f4b38bb8
José Carlos Ferreira Júnior <dev.josejunior@gmail.com>
2020-05-24 19:37:03 -0300

    refactor(api): add hash provider to handle comparation of passwords

--- backend/src/modules/users/services/AuthenticateUserService.ts
+++ backend/src/modules/users/services/AuthenticateUserService.ts
@@ -6,6 +6,7 @@ import authConfig from '@config/auth';
 
 import AppError from '@shared/errors/AppError';
 
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 import User from '@modules/users/infra/typeorm/entities/User';
 import IUsersRepository from '../repositories/IUsersRepository';
 
@@ -24,6 +25,9 @@ class AuthenticateUserService {
   constructor(
     @inject('UsersRepository')
     private usersRepository: IUsersRepository,
+
+    @inject('HashProvider')
+    private hashProvider: IHashProvider,
   ) {}
 
   public async execute({ email, password }: IRequest): Promise<IResponse> {
@@ -33,7 +37,10 @@ class AuthenticateUserService {
       throw new AppError('Incorrect email/password combination!', 401);
     }
 
-    const passwordMatched = await compare(password, user.password);
+    const passwordMatched = await this.hashProvider.compareHash(
+      password,
+      user.password,
+    );
 
     if (!passwordMatched) {
       throw new AppError('Incorrect email/password combination!', 401);
