9d731d3571206e06f6166f16661c24af9a8096b3
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-05-25 17:41:19 -0300

    refactor(api): add fake to the name of fake Users Repository

--- backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
+++ backend/src/modules/users/repositories/fakes/FakeUsersRepository.ts
@@ -4,7 +4,7 @@ import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
 
 import User from '../../infra/typeorm/entities/User';
 
-class UsersRepository implements IUsersRepository {
+class FakeUsersRepository implements IUsersRepository {
   private users: User[] = [];
 
   public async create(userData: ICreateUserDTO): Promise<User> {
@@ -38,4 +38,4 @@ class UsersRepository implements IUsersRepository {
   }
 }
 
-export default UsersRepository;
+export default FakeUsersRepository;
