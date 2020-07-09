4125e3b95f3a14213f7cdbe40f9190d9769310d2
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-09 19:54:44 -0300

    feat: add reset password routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
+import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
 
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
+routes.use('/password', passwordRoutes);
 
 export default routes;
