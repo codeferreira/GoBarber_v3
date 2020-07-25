c14566f1fc05ddf80fa8f4990fb31655a986f1e9
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-24 21:52:29 -0300

    chore: add profile routes

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointm
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
+import profileRoutes from '@modules/users/infra/http/routes/profile.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/appointments', appointmentsRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/password', passwordRoutes);
+routes.use('/profile', profileRoutes);
 
 export default routes;
