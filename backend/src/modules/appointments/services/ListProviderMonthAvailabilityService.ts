d279ee629037fce4400a4ce92317d3dae7a642c5
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-27 08:55:17 -0300

    feat: add a service to handle list all available appointments from a provider

---
+++ backend/src/modules/appointments/services/ListProviderMonthAvailabilityService.ts
@@ -0,0 +1,40 @@
+import { injectable, inject } from 'tsyringe';
+
+import IAppointmentsRepository from '@modules/appointments/repositories/IAppointmentsRepository';
+
+import User from '@modules/users/infra/typeorm/entities/User';
+
+interface IRequest {
+  provider_id: string;
+  month: number;
+  year: number;
+}
+
+type IResponse = Array<{
+  day: number;
+  available: boolean;
+}>;
+
+@injectable()
+class ListProvidersService {
+  constructor(
+    @inject('AppointmentsRepository')
+    private appointmentsRepository: IAppointmentsRepository,
+  ) {}
+
+  public async execute({
+    provider_id,
+    year,
+    month,
+  }: IRequest): Promise<IResponse> {
+    const appointments = await this.appointmentsRepository.findAllInMonthFromProvider(
+      { provider_id, year, month },
+    );
+
+    console.log(appointments);
+
+    return [{ day: 1, available: true }];
+  }
+}
+
+export default ListProvidersService;
