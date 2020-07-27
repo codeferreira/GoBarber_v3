47894f9f815e95852dfd678c371a97c4171f84ca
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-27 08:54:35 -0300

    chore: change imported controller file name

--- backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ backend/src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
-import AppointmentsController from '../controllers/AppointmentsContrroller';
+import AppointmentsController from '../controllers/AppointmentsController';
 
 const appointmentsRoutes = Router();
 const appointmentsController = new AppointmentsController();
