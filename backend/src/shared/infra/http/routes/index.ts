e4a98c60228f3c4bf5adaf02e5ac3a53a0b9591c
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-27 08:55:28 -0300

    chore: add providers route to main router

--- backend/src/shared/infra/http/routes/index.ts
+++ backend/src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import appointmentsRoutes from '@modules/appointments/infra/http/routes/appointments.routes';
+import providersRoutes from '@modules/appointments/infra/http/routes/providers.routes';
 import usersRoutes from '@modules/users/infra/http/routes/users.routes';
 import sessionsRoutes from '@modules/users/infra/http/routes/sessions.routes';
 import passwordRoutes from '@modules/users/infra/http/routes/password.routes';
@@ -8,6 +9,7 @@ import profileRoutes from '@modules/users/infra/http/routes/profile.routes';
 const routes = Router();
 
 routes.use('/appointments', appointmentsRoutes);
+routes.use('/providers', providersRoutes);
 routes.use('/users', usersRoutes);
 routes.use('/sessions', sessionsRoutes);
 routes.use('/password', passwordRoutes);
