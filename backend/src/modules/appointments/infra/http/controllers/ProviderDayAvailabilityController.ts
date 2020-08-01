f3b9d8ca747fe28b29487d67c58dcfc61ec7e1b0
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-07-31 22:55:11 -0300

    feat: add routes to handle availability of month and day

---
+++ backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProviderDayAvailabilityService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProviderDayAvailabilityController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { provider_id } = request.params;
+    const { year, day, month } = request.body;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayAvailabilityService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      day,
+      month,
+      year,
+      provider_id,
+    });
+
+    return response.json(availability);
+  }
+}
