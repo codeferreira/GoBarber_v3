d70511499ad2a948fffceadd91e7786037aa9172
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-08-07 14:56:58 -0300

    chore: add reset and forgot password routes

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,11 +5,15 @@ import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
+import ForgotPassword from '../pages/ForgotPassword';
+import ResetPassword from '../pages/ResetPassword';
 import Dashboard from '../pages/Dashboard';
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={SignIn} />
+    <Route path="/forgot-password" exact component={ForgotPassword} />
+    <Route path="/reset-password" exact component={ResetPassword} />
     <Route path="/signup" component={SignUp} />
     <Route path="/dashboard" component={Dashboard} isPrivate />
   </Switch>
