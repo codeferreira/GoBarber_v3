b2a030236eb8277c6b87cd54485551257d315dd3
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-08-07 14:55:01 -0300

    chore: add loading to button

--- frontend/src/components/Button/index.tsx
+++ frontend/src/components/Button/index.tsx
@@ -2,11 +2,13 @@ import React, { ButtonHTMLAttributes } from 'react';
 
 import { Container } from './styles';
 
-type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement>;
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  loading?: boolean;
+};
 
-const Button: React.FC<ButtonProps> = ({ children, ...rest }) => (
-  <Container type="button" {...rest}>
-    {children}
+const Button: React.FC<ButtonProps> = ({ children, loading, ...rest }) => (
+  <Container disabled={loading} type="button" {...rest}>
+    {loading ? 'Carregando...' : children}
   </Container>
 );
 
