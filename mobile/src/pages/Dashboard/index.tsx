14eec3e3bde5393642476d97313fe4771b953c07
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-08-10 02:35:27 -0300

    feat: add dashboard proviers list

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -1,10 +1,118 @@
-import React from 'react';
-import { View } from 'react-native';
+import React, { useCallback, useEffect, useState } from 'react';
+import { Alert } from 'react-native';
 
-// import { Container } from './styles';
+import Icon from 'react-native-vector-icons/Feather';
+import { useNavigation } from '@react-navigation/native';
+import api from '../../services/api';
+import { useAuth } from '../../hooks/Auth';
+
+import {
+  Container,
+  Header,
+  HeaderTitle,
+  Username,
+  ProfileButton,
+  UserAvatar,
+  ProvidersList,
+  ProvidersListTitle,
+  ProviderContainer,
+  ProviderAvatar,
+  ProviderInfo,
+  ProviderName,
+  ProviderMeta,
+  ProviderMetaText,
+} from './styles';
+
+export interface Provider {
+  id: string;
+  name: string;
+  avatar_url: string;
+}
 
 const Dashboard: React.FC = () => {
-  return <View />;
+  const { user, signOut } = useAuth();
+  const { navigate } = useNavigation();
+
+  const [providers, setProviders] = useState<Provider[]>([]);
+
+  useEffect(() => {
+    async function loadProviders(): Promise<void> {
+      try {
+        const response = await api.get('/providers');
+
+        setProviders(response.data);
+      } catch (err) {
+        Alert.alert(
+          'Erro ao carregar',
+          'Ocorreu um erro ao carregar os dados dos provedores',
+        );
+      }
+    }
+
+    loadProviders();
+  }, []);
+
+  const navigateToProfile = useCallback(() => {
+    // navigate('Profile');
+    signOut();
+  }, [signOut]);
+
+  const navigateToCreateAppointment = useCallback(
+    (providerId: string) => {
+      navigate('CreateAppointment', { providerId });
+    },
+    [navigate],
+  );
+
+  return (
+    <Container>
+      <Header>
+        <HeaderTitle>
+          Bem vindo, {'\n'}
+          <Username>{user.name}</Username>
+        </HeaderTitle>
+
+        <ProfileButton onPress={navigateToProfile}>
+          <UserAvatar source={{ uri: user.avatar_url }} />
+        </ProfileButton>
+      </Header>
+
+      <ProvidersList
+        data={providers}
+        keyExtractor={(provider) => provider.id}
+        ListHeaderComponent={
+          <ProvidersListTitle>Cabeleireiros</ProvidersListTitle>
+        }
+        renderItem={({ item: provider }) => (
+          <ProviderContainer
+            onPress={() => navigateToCreateAppointment(provider.id)}
+          >
+            <ProviderAvatar
+              source={{
+                uri:
+                  provider.avatar_url ||
+                  'https://api.adorable.io/avatars/150/default-avatar.png',
+              }}
+            />
+
+            <ProviderInfo>
+              <ProviderName>{provider.name}</ProviderName>
+
+              <ProviderMeta>
+                <Icon name="calendar" size={14} color="#ff9000" />
+                <ProviderMetaText>Segunda à sexta</ProviderMetaText>
+              </ProviderMeta>
+
+              <ProviderMeta>
+                <Icon name="clock" size={14} color="#ff9000" />
+                <ProviderMetaText>8h às 17h</ProviderMetaText>
+              </ProviderMeta>
+            </ProviderInfo>
+          </ProviderContainer>
+        )}
+      />
+    </Container>
+  );
 };
 
 export default Dashboard;
