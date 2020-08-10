b9110aef6710f9db31103c7deb545e665b213272
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-08-10 14:39:01 -0300

    chore: remove signout option

--- mobile/src/pages/Dashboard/index.tsx
+++ mobile/src/pages/Dashboard/index.tsx
@@ -30,7 +30,7 @@ export interface Provider {
 }
 
 const Dashboard: React.FC = () => {
-  const { user, signOut } = useAuth();
+  const { user } = useAuth();
   const { navigate } = useNavigation();
 
   const [providers, setProviders] = useState<Provider[]>([]);
@@ -53,9 +53,8 @@ const Dashboard: React.FC = () => {
   }, []);
 
   const navigateToProfile = useCallback(() => {
-    // navigate('Profile');
-    signOut();
-  }, [signOut]);
+    navigate('Profile');
+  }, [navigate]);
 
   const navigateToCreateAppointment = useCallback(
     (providerId: string) => {
--- mobile/src/pages/Dashboard/styles.ts
+++ mobile/src/pages/Dashboard/styles.ts
@@ -1,6 +1,6 @@
 import styled from 'styled-components/native';
 import { FlatList } from 'react-native';
-import { isIphoneX, getStatusBarHeight } from 'react-native-iphone-x-helper';
+import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import { RectButton } from 'react-native-gesture-handler';
 import { Provider } from './index';
@@ -11,7 +11,7 @@ export const Container = styled.View`
 
 export const Header = styled.View`
   padding: 24px;
-  padding-top: ${isIphoneX() ? getStatusBarHeight() + 24 : 24}px;
+  padding-top: ${getStatusBarHeight() + 24}px;
   background: #28262e;
 
   flex-direction: row;
