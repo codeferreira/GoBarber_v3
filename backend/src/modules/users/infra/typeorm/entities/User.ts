f6bab77e80bb1a21b507a4b0e982c76c9b5d334a
Jose Carlos Ferreira Junior <dev.josejunior@gmail.com>
2020-08-09 22:17:48 -0300

    chore: change avatar files url

--- backend/src/modules/users/infra/typeorm/entities/User.ts
+++ backend/src/modules/users/infra/typeorm/entities/User.ts
@@ -42,7 +42,7 @@ class User {
 
     switch (uploadConfig.driver) {
       case 'disk':
-        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+        return `${process.env.APP_API_URL}/files/uploads/${this.avatar}`;
       case 's3':
         return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
       default:
